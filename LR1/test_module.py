b0e9db50a8541ac3c702e647cb96a573dd0c2989
Svatoslav <74541429+svgab@users.noreply.github.com>
2025-09-16 12:09:47 +0300

    Update test_module.py

--- LR1/test_module.py
+++ LR1/test_module.py
@@ -1,12 +1,21 @@
 import unittest
 import first
 
+
 class TestMySolution(unittest.TestCase):
 
-    def test_same_lst_elems(self):
-        self.assertEqual(first.Solution().two_sum([2, 7, 11, 15], 9), (0, 1))
-        self.assertEqual(first.Solution().two_sum([3, 3, 3, 3], 6), (0, 1))
-        self.assertEqual(first.Solution().two_sum([16, 2, 15, 89], 105), (0, 3))
+    def test(self):
+        self.assertEqual(first.Solution().two_sum([2, 7, 11, 15], 9), [0, 1])
+
+    def test_same_elems(self):
+        self.assertEqual(first.Solution().two_sum([3, 3, 3, 3], 6), [0, 1])
+
+    def test_big_number(self):
+        self.assertEqual(first.Solution().two_sum([16, 2, 15, 89], 105),
+                         [0, 3])
+        self.assertEqual(first.Solution().two_sum([306, 2, 15, 89], 308),
+                         [0, 1])
+
 
 if __name__ == '__main__':
     unittest.main()
